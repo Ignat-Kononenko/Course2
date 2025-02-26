8542882598c42cbcfc124d5cf7b6348d87236213
Ignat <151752700+Ignat-Kononenko@users.noreply.github.com>
2025-02-26 23:55:01 +0300

    Add files via upload

---
+++ 4semester/mathematicalProgramming/lab1/Lab1/Lab1/stdafx.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Auxil.h"
+#include <iostream>
+#include <ctime>
+#include <locale>                          // вспомогательные функции 
+#include "Fibonachi.h"
+
+using namespace std;
