5dc8279d2cde3575547b58202cc9725145285268
Ignat <151752700+Ignat-Kononenko@users.noreply.github.com>
2025-02-28 19:49:05 +0300

    Add files via upload

---
+++ 4semester/mathematicalProgramming/lab3/Lab3/Lab3/Source.cpp
@@ -0,0 +1,41 @@
+// --- main 
+#include <iostream>
+#include <iomanip> 
+#include "Salesman.h"
+#include<tchar.h>
+#define N 5
+int _tmain(int argc, _TCHAR* argv[])
+{
+    setlocale(LC_ALL, "rus");
+    int d[N][N] = { //0   1    2    3     4        
+                  {  INF,  12, 27,  INF,   6},    //  0
+                  { 6,   INF,  21,  62,  78},    //  1
+                  { 8,  18,   INF,  86,   55},    //  2 
+                  { 23,  52,  24,   INF,   18},    //  3
+                  { 87,  72,  52,  19,    INF} };   //  4  
+    int r[N];                     // результат 
+    int s = salesman(
+        N,          // [in]  количество городов 
+        (int*)d,          // [in]  массив [n*n] расстояний 
+        r           // [out] массив [n] маршрут 0 x x x x  
+
+    );
+    std::cout << std::endl << "-- Задача коммивояжера -- ";
+    std::cout << std::endl << "-- количество  городов: " << N;
+    std::cout << std::endl << "-- матрица расстояний : ";
+    for (int i = 0; i < N; i++)
+    {
+        std::cout << std::endl;
+        for (int j = 0; j < N; j++)
+
+            if (d[i][j] != INF) std::cout << std::setw(3) << d[i][j] << " ";
+
+            else std::cout << std::setw(3) << "INF" << " ";
+    }
+    std::cout << std::endl << "-- оптимальный маршрут: ";
+    for (int i = 0; i < N; i++) std::cout << r[i] << "-->"; std::cout << 0;
+    std::cout << std::endl << "-- длина маршрута     : " << s;
+    std::cout << std::endl;
+    system("pause");
+    return 0;
+}
